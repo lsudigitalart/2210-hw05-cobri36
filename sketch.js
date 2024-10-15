8fc2bbc44dd38a6375cd57c8fddd124564656134
casobri17 <casobri17@gmail.com>
2024-10-15 09:57:33 -0500

    ononio

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 function setup()
-{createCanvas(400,400);
-background(220);
-textSize(32);
-text('im gonna kms plz work', 50, 200);
+{
+    createCanvas(400,400);
+    background(220);
+}
+function draw()
+{
+    textSize(32);
+    text('im gonna kms plz work', 50, 200);
 }
